551b6272dc76220d037f334be3fa358416249ab3
鈴木太郎 <suzukitarou@suzukitarounoMacBook-Pro.local>
2024-04-15 00:00:55 +0900

    fix(atcoder): abc349-d

--- atcoder/abc349/d/main.py
+++ atcoder/abc349/d/main.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 L, R = map(int, input().split())
 
+answers = [] # (left, right)のリスト
 
-def s_func(l, r, counter=0):
-    if l % 2 == 1:
-        counter += 1
-        s_func(l+1, r, counter)
-    else:
-        # lが偶数のとき、rが2の何乗かでiが決まる
-        pass
+while L < R:
+    i = 0
+    # S(l, r) = S(pow(2, i)*j, pow(2, i)*(j+1))なので、
+    # lがpow(2, i)で割り切れる かつ
+    # rがl+pow(2, i)以下
+    # を満たす最大のiが求める区間
+    while (L % pow(2, i+1) == 0) and (L + pow(2, i+1) <= R):
+        i += 1
+    answers.append([L, L + pow(2, i)])
+    L += pow(2, i)
+
+print(len(answers))
+for l, r in answers:
+    print(l, r)

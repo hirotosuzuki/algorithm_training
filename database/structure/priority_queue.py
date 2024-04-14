78fbf4af4d4d9ecdb87988ac1f5150e6191425dc
鈴木太郎 <suzukitarou@suzukitarounoMacBook-Pro.local>
2024-04-14 23:32:56 +0900

    feat(database): priority_queue

--- database/structure/priority_queue.py
+++ database/structure/priority_queue.py
@@ -1,11 +1,5 @@
 import abc
-
-"""binary heapを使った優先度付きキュー
-挿入：O(logN)
-削除：O(logN)
-最小値取得：O(1)
-"""
-
+import heapq
 
 class PriorityQueueADT(metaclass=abc.ABCMeta):
     @abc.abstractmethod
@@ -32,8 +26,45 @@ class PriorityQueueADT(metaclass=abc.ABCMeta):
         raise NotImplementedError()
 
 class PriorityQueueUsingBinaryHeap(PriorityQueueADT):
-    pass
+    """binary heapを使った優先度付きキュー
+    挿入：O(logN)
+    削除：O(logN)
+    最小値取得：O(1)
+    """
+    def __init__(self, values: list[int]):
+        self.heap = values
+        heapq.heapify(self.heap)
+    
+    def insert(self, key: int, value: int) -> None:
+        heapq.heappush(self.heap, value)
+    
+    def get_min(self) -> int | None:
+        if len(self.heap) == 0:
+            return None
+        return self.heap[0]
+    
+    def delete_min(self) -> None:
+        heapq.heappop(self.heap)
 
+    def kth_smallest(self, k: int) -> int:
+        return heapq.nsmallest(k, self.heap)[-1]
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: list[int]):
+        self.pool = nums
+        self.k = k
+        heapq.heapify(self.pool)
+        # kより要素数が多い場合は消す
+        while len(self.pool) > k:
+            heapq.heappop(self.pool)
+
+    def add(self, val: int) -> int:
+        if len(self.pool) < self.k:
+            heapq.heappush(self.pool, val)
+        elif val > self.pool[0]:
+            heapq.heapreplace(self.pool, val)
+        return self.pool[0]
 
 if __name__ == "__main__":
     pq = PriorityQueueUsingBinaryHeap()
@@ -47,3 +78,10 @@ if __name__ == "__main__":
     print(pq.get_min())  # 30
     pq.delete_min()
     print(pq.get_min())  # None
+
+    # https://docs.python.org/ja/3/library/queue.html
+    # from queue import PriorityQueue
+
+    # pq = PriorityQueue()
+    # pq.put(1)
+    # pq.get()
